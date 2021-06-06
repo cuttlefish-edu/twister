dd60894ac527ea384b410b56cc6ba765a36a2799
Graham <gkgoat6700@gmail.com>
2021-06-05 20:30:16 -0400

    stuff2

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
 const path = require('path');
 
 module.exports = {
-  entry: './jjencode.js',
+  entry: './jjencode.mjs',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  resolve: {
+	preferRelative: true
+  }
 };
